3c45cd0aee5de7de30cfa08c89190fc06ec4b2c7
Hadeer <hadeerelhussen1111@gmail.com>
2022-09-13 15:57:13 +0200

    Commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
+import { Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(private fb:FormBuilder){ }
+
+    userprofileForm = this.fb.group({
+      firstName:['', Validators.required],
+      lastName:[''],
+
+      address: this.fb.group({
+        address1:[''],
+        address2:[''],
+        state:[''],
+        zip:['']
+      })
+
+    });
+
+    onSubmit(){
+      console.warn(this.userprofileForm.value)
+    }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule }   from '@angular/forms';
+import { FormBuilder} from '@angular/forms';
+import { Validators} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormBuilder,
+    Validators
   ],
   providers: [],
   bootstrap: [AppComponent]
